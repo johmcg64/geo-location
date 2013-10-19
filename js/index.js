eb592dddf7fc9aa3098591f1667d720a7b8a9c4a
johmcg64 <johmcg64@gmail.com>
2013-10-19 17:26:25 -0600

    Update index.js
    e

--- js/index.js
+++ js/index.js
@@ -41,25 +41,26 @@ var app = {
     	//navigator.notification.alert("Hi John",function(){},"title","Done")
     	
     	
-    		var onSuccess = function(position) {
-          navigator.notification.alert('Latitude: '          + position.coords.latitude          + '\n' +
+    	var onSuccess = function(position) {
+    alert('Latitude: '          + position.coords.latitude          + '\n' +
           'Longitude: '         + position.coords.longitude         + '\n' +
           'Altitude: '          + position.coords.altitude          + '\n' +
           'Accuracy: '          + position.coords.accuracy          + '\n' +
           'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
           'Heading: '           + position.coords.heading           + '\n' +
           'Speed: '             + position.coords.speed             + '\n' +
-          'Timestamp: '         + position.timestamp                + '\n',function(){},"title","Done");
-    }
+          'Timestamp: '         + new Date(position.timestamp)      + '\n');
+};
 
 // onError Callback receives a PositionError object
 //
-       function onError(error) {
-         navigator.notification.alert('code: '    + error.code    + '\n' +'message: ' + error.message + '\n',function(){},"title","Done");
-       }
-    	
+function onError(error) {
+    alert('code: '    + error.code    + '\n' +
+          'message: ' + error.message + '\n');
+}
 
-       navigator.geolocation.getCurrentPosition(onSuccess, onError);
+navigator.geolocation.getCurrentPosition(onSuccess, onError);
+
 
    
     },
