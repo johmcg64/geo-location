f422ac1ec81637313112d0fa1a599567e9074bc5
johmcg64 <johmcg64@gmail.com>
2013-10-24 16:38:39 -0600

    Update index.js
    e

--- js/index.js
+++ js/index.js
@@ -41,7 +41,21 @@ var app = {
     	//navigator.notification.alert("Hi John",function(){},"title","Done")
     	//alert("Hi John")
   
-  alert(1)  	
+  alert(1)  
+  
+  function onSuccess(acceleration) {
+    alert('Acceleration X: ' + acceleration.x + '\n' +
+          'Acceleration Y: ' + acceleration.y + '\n' +
+          'Acceleration Z: ' + acceleration.z + '\n' +
+          'Timestamp: '      + acceleration.timestamp + '\n');
+};
+
+function onError() {
+    alert('onError!');
+};
+
+navigator.accelerometer.getCurrentAcceleration(onSuccess, onError);
+
     	/*
     	var onSuccess = function(position) {
     alert('Latitude: '          + position.coords.latitude          + '\n' +
